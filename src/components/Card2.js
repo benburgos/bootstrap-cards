279aa07f785b1f37d9fed0ba5c5b19e538ef4015
Ben <ben.a.burgos@gmail.com>
2022-04-30 15:17:30 -0700

    refactors code

--- src/components/Card2.js
+++ src/components/Card2.js
@@ -1,21 +1,33 @@
 import React from 'react'
+import CardImage from './CardImage'
+import CardBody from './CardBody'
 
 const Card2 = () => {
   return (
     <div className="card" style={{width: "18rem"}}>
-        <img
+        {/* <img
           src="https://images.unsplash.com/photo-1498712964741-5d33ab9e5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=600"
          className="card-img-top"
           alt="..."
+        /> */}
+        <CardImage 
+          imgSrc="https://images.unsplash.com/photo-1560703650-ef3e0f254ae0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" 
+          imgAlt="..." 
+          imgStyle="card-img-top" 
         />
-        <div className="card-body">
+        {/* <div className="card-body">
           <h5 className="card-title">Card 2</h5>
           <p className="card-text">
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </p>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          </p>
           <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
+        </div> */}
+        <CardBody 
+          cardTitle="Card 2" 
+          cardText="Some quick example text to build on the card title and make up the
+          bulk of the card's content." 
+        />
       </div>
   )
 }
